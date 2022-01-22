ef15cf5a3495ad5820065192c18a686ad52f4cfe
NirZamir <nirozamir@gmail.com>
2022-01-22 20:00:14 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace CV_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1121, 509);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(1121, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(0, 509);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1121, 509);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CV_
+{
+    public partial class Form1 : Form
+    {
+        Bitmap ToGray;
+        Bitmap VerticalEdge;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ToGray = new Bitmap(@"C:\Users\PC\Downloads\20201229_151533.jpg");
+            VerticalEdge = new Bitmap(@"C:\Users\PC\Downloads\20201229_151533.jpg");
+            pictureBox1.Image = ToGrayScale(ToGray);
+            pictureBox2.Image = Verticaledge(ToGrayScale(VerticalEdge));
+        }
+        static Bitmap ToGrayScale(Bitmap bitmap)
+        {
+            int avg;
+            Color color;
+            int[] RGB = new int[3];
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for(int y = 0; y<bitmap.Height; y++ )
+                {
+                    color = bitmap.GetPixel(x,y);
+                    RGB[0] = color.R;
+                    RGB[1] = color.G;
+                    RGB[2] = color.B;
+                    avg = Sum(RGB);
+                    bitmap.SetPixel(x, y, Color.FromArgb(avg, avg, avg));
+                }
+                
+            }
+            return bitmap;
+        }
+        static Bitmap Verticaledge(Bitmap bitmap /* must be grayScale */)
+        {
+            int convolution = 0;
+            int sumLeft = 0;
+            int sumRight = 0;
+            Bitmap _bitmap = new Bitmap(bitmap.Width, bitmap.Height);
+            for(int x = 1; x < bitmap.Width - 1; x ++)
+            {
+                for(int y = 1; y < bitmap.Height-1; y++ )
+                {
+                    sumLeft = 0;
+                    sumRight = 0;
+                    for(int i = -1; y-i<bitmap.Height && i<=1; i++)
+                    {
+                        sumLeft += bitmap.GetPixel(x - 1, y+i).R;
+                    }
+                    for (int i = -1; y - i < bitmap.Height && i<=1; i++)
+                    {
+                        sumRight += bitmap.GetPixel(x + 1, y+i).R;
+                    }
+                    convolution =Math.Abs(sumRight-sumLeft);
+                    if(convolution>255)
+                    {
+                        convolution = 255;
+                    }
+                    _bitmap.SetPixel(x, y, Color.FromArgb(convolution, convolution, convolution));
+                }
+            }
+            return _bitmap;
+        }
+        static int Sum(int[] Array)
+        {
+            int Container=0;
+            for(int i = 0; i <Array.Length;i++)
+            {
+                Container += Array[i];
+            }
+            return Container / Array.Length;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
